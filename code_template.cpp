3e6edc59c58da2206f7619285a77f2ce974e4d15
sxycrjp <r_cestlavie@163.com>
2016-09-24 23:22:30 +0800

    Create code_template.cpp

---
+++ code_template.cpp
@@ -0,0 +1,46 @@
+#include<fstream>
+#include<iostream>
+#include<sstream>
+
+#include<stdio.h>
+#include<limits.h>
+#include<math.h>
+#include<iomanip>
+#include<cstdlib>
+
+#include<string>
+#include<cstring>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<algorithm>
+
+using namespace std;
+
+#define xx         first
+#define yy         second
+#define pb         push_back
+#define mp         make_pair
+#define LL         long long
+#define inf        INT_MAX/3
+#define mod        1000000007ll
+#define PI         acos(-1.0)
+#define linf       (1ll<<60)-1
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     ((A).begin(), (A).end())
+#define set0(ar)   memset(ar,0,sizeof ar)
+#define vsort(v)   sort(v.begin(),v.end())
+#define setinf(ar) memset(ar,126,sizeof ar)
+const int _  = (int)(1e4 + 10);
+const int __ = (int)(1e6 + 10);
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout << "Hello World!" << endl;
+    return 0;
+}
